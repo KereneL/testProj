8b39096f1853f6a6e8b56643647ce0ef10e5bc2b
KereneL <kerenel@gmail.com>
2021-07-05 12:31:54 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -1,53 +1,102 @@
+// on page load - call createAddLink();
+window.onload = function() {
+    createAddLink();
+  };
+
 function createForm() {
+    //create <form> element
+    //set id = form
+    //onsubmit = addListItem() and "return false;" to avoid page refreshing
     var form = document.createElement("form");
     form.setAttribute("id", "form");
     form.setAttribute("onsubmit", "addListItem(); return false;");
 
+    //create <input> element of type text
+    //set id = text-id
+    //set type, name and placeholder
+    //add element as child to form
     var inputName = document.createElement("input");
-    inputName.setAttribute("id", "text-id");
     inputName.setAttribute("type", "text");
-    inputName.setAttribute("name", "Name:");
-    inputName.setAttribute("placeholder", "Enter Name...");
+    inputName.setAttribute("id", "text-id");
+    inputName.setAttribute("name", "name");
+    inputName.setAttribute("placeholder", "Enter Text...");
     form.appendChild(inputName);
 
+    //create <input> element of type button
+    //set onclick event - addListItem()
+    //set button text = Submit
+    //add element as child to form
     var inputSubmit = document.createElement("input");
     inputSubmit.setAttribute("type", "button");
     inputSubmit.setAttribute("onclick", "addListItem()");
-    inputSubmit.setAttribute("value", "Submit");
+    inputSubmit.setAttribute("value", "Add");
     form.appendChild(inputSubmit);
 
+    //create <input> element of type button
+    //set onclick event - removeForm()
+    //set button text = Cancel
+    //add element as child to form
     var inputCancel = document.createElement("input");
     inputCancel.setAttribute("type", "button");
     inputCancel.setAttribute("onclick", "removeForm()");
     inputCancel.setAttribute("value", "Cancel");
     form.appendChild(inputCancel);
 
+    //add form as a child to 'container' (which is present in HTML)
     document.getElementById("container").appendChild(form);
 
+    //when the form opens, remove the link
     removeAddLink();  
+    //set focus to the name field
     inputName.focus();
 }
 
 function removeForm() {
+    //select form element
+    //remove element from parent element
+    //create the "add new" link again using another function
     var form = document.getElementById("form");
     form.parentNode.removeChild(form);
     createAddLink();
 }
 
 function createAddLink() {
+    //create anchor (<a>) element
+    //add href attribute as "javascript:void(0)" so the page won't reload on click
+    //add onclick event - createForm()
+    //add the link to the end of container div
     var linkCreateNew = document.createElement("a");
     linkCreateNew.setAttribute("href", "javascript:void(0)");
     linkCreateNew.setAttribute("onclick", "createForm()");
     linkCreateNew.innerHTML = "Create New...";
-    document.getElementById("container").appendChild(linkCreateNew);
+
+    //create a new <div> element
+    //add the <a> element linkCreateNew to that <div>
+    //add the <div> element newDiv to our container
+    var newDiv = document.createElement("div");
+    newDiv.appendChild(linkCreateNew);
+    document.getElementById("container").appendChild(newDiv);
+
+    //set focus to link
+    linkCreateNew.focus();
 }
 
 function removeAddLink(){
+    //select anchor (<a>) element
+    //select <a>'s parent (which is a <div>)
+    //remove element from parent element
     var link = document.getElementsByTagName("a")[0];
-    link.parentNode.removeChild(link);
+    var div = link.parentNode;
+    div.parentNode.removeChild(div);
 }
 
 function addListItem(){
+    //get the text from the form's textbox
+    //if the text isn't empty -
+    //add a new <li> element
+    //add the text to that <li>
+    //add <li> to list (where id=list)
+    //after we added an item, remove the form using another function - removeForm(), which also calls the createAddLink() function
     var text = document.getElementById("text-id").value;
     if (text.length > 0){
         var added = document.createElement("li");
