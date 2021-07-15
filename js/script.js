564acb7bff850c5653e8c579e8d35f2c62205232
KereneL <kerenel@gmail.com>
2021-07-15 12:06:02 +0300

    JB Project 01

--- js/script.js
+++ js/script.js
@@ -1,148 +1,107 @@
-// on page load - call createAddLink();
-window.onload = function () {
-    createAddLink();
-    seed();
+var inptTxt = document.getElementById("inputText");
+var notesArr = [];
 
-};
+seed();
+clearForm();
 
 function seed() {
-    var arr = ["🐶🐱🐷🦆", "Hello World.js", "🏈🏐🏀⚽", "🚗🚛🚙🚓"];
-    for (let i = 0; i < arr.length; i++) {
-        addListItem(arr[i]);
+    for (let i = 0; i < 5; i++) {
+
+        notesArr.push({
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit tincidunt sem vel aliquam. Nulla quam urna, convallis quis ultricies in, aliquet vitae urna. Sed ultrices, odio et dapibus luctus, quam massa tincidunt risus, ac condimentum urna diam eu ligula. Proin vulputate nunc sed massa malesuada, et lacinia sem malesuada. Curabitur ultrices at leo sit amet laoreet. Donec eget orci maximus, convallis nibh efficitur, gravida odio. Suspendisse a lorem leo. Sed accumsan erat et neque pulvinar, eu scelerisque magna euismod. Etiam quis quam ac nulla varius tristique. Aenean at orci consectetur, viverra eros quis, interdum erat. Aenean finibus, ipsum sit amet consectetur dapibus, arcu orci ullamcorper libero, vel mattis sem nunc nec urna. Sed sollicitudin eros vel faucibus rhoncus.`,
+            dateObj: {
+                date: "1992-02-24",
+                time: "12:21"
+            }
+        });
     }
+    saveArr();
+    loadArr();
 }
 
-function createForm() {
-    //create <form> element
-    //set id = form, and class as style_1
-    //onsubmit = extractTextFromForm() and "return false;" to avoid page refreshing
-    //apply class for styling
-    //disable auto-complete, just because
-    var form = document.createElement("form");
-    form.setAttribute("id", "form");
-    form.setAttribute("onsubmit", "extractTextFromForm(); return false;");
-    form.setAttribute("class", "style_1");
-    form.setAttribute("autocomplete", "off");
-
-    //create <input> element of type text
-    //set id = text-id
-    //set type, name and placeholder
-    //add element as child to form
-    var inputName = document.createElement("input");
-    inputName.setAttribute("type", "text");
-    inputName.setAttribute("id", "text-id");
-    inputName.setAttribute("name", "name");
-    inputName.setAttribute("placeholder", "Enter Text...");
-    form.appendChild(inputName);
-
-    //create <input> element of type button
-    //set onclick event - extractTextFromForm()
-    //set button text = Submit
-    //add element as child to form
-    var inputSubmit = document.createElement("input");
-    inputSubmit.setAttribute("type", "button");
-    inputSubmit.setAttribute("onclick", "extractTextFromForm();");
-    inputSubmit.setAttribute("value", "Add");
-    form.appendChild(inputSubmit);
-
-    //create <input> element of type button
-    //set onclick event - removeForm()
-    //set button text = Cancel
-    //add element as child to form
-    var inputCancel = document.createElement("input");
-    inputCancel.setAttribute("type", "button");
-    inputCancel.setAttribute("onclick", "removeForm()");
-    inputCancel.setAttribute("value", "Cancel");
-    form.appendChild(inputCancel);
-
-    //add form as a child to 'container' (which is present in HTML)
-    document.getElementById("container").appendChild(form);
-
-    //when the form opens, remove the link
-    removeAddLink();
-    //set focus to the name field
-    inputName.focus();
+function saveArr() {
+    localStorage.setItem("notes", JSON.stringify(notesArr));
 }
 
-function removeForm() {
-    //select form element
-    //remove element from parent element
-    //create the "add new" link again using another function
-    var form = document.getElementById("form");
-    form.parentNode.removeChild(form);
-    createAddLink();
+function loadArr() {
+    var tempLoaded = JSON.parse(localStorage.getItem("notes"));
+    console.log(tempLoaded);
+
+    tempLoaded.forEach(note => {
+        addNewListItem(note.text, note.dateObj);
+    });
 }
 
-function createAddLink() {
-    //create anchor (<div>) element
-    //add href attribute as "javascript:void(0)" so the page won't reload on click
-    //add onclick event - createForm()
-    //add class - button
-    //add the label
-    var linkCreateNew = document.createElement("div");
-    linkCreateNew.setAttribute("href", "javascript:void(0)");
-    linkCreateNew.setAttribute("onclick", "createForm()");
-    linkCreateNew.setAttribute("class", "style_1");
-    linkCreateNew.innerHTML = "Add More...";
-
-    //create a new <div> element
-    //add the <a> element linkCreateNew to that <div>
-    //add the <div> element newDiv to our container
-    var newDiv = document.createElement("div");
-    newDiv.appendChild(linkCreateNew);
-    document.getElementById("container").appendChild(newDiv);
-
-    //set focus to link
-    linkCreateNew.focus();
+function resetTime() {
+    let now = new Date();
+    document.getElementById("inputDate").value = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, 0)}-${(now.getDate()).toString().padStart(2, 0)}`;
+    document.getElementById("inputTime").value = `${now.getHours().toString().padStart(2, 0)}:${now.getMinutes().toString().padStart(2, 0)}`;
 }
 
-function removeAddLink() {
-    //select elements with "style_1" class
-    //select elements parent (which is a <div>)
-    //remove element from parent element
-    var link = document.getElementsByClassName("style_1")[0];
-    var div = link.parentNode;
-    div.parentNode.removeChild(div);
+function clearForm() {
+    inptTxt.value = "";
+    resetTime();
+    inputText.focus();
 }
 
-function extractTextFromForm() {
-    //get the text from the form's textbox
-    //if it's not empty -
-    //call addListItem() with 'text' as an argument
-    var text = document.getElementById("text-id").value;
-    if (text.length > 0) {
-        addListItem(text);
-    }
+function addNewItem() {
+    var text = inptTxt.value;
+    if (text == "") return;
+
+    let datePicker = document.getElementById("inputDate").value;
+    let timePicker = document.getElementById("inputTime").value;
+    var dateObj = {
+        date: datePicker,
+        time: timePicker
+    };
+
+    notesArr.push({
+        text: text,
+        dateObj: dateObj
+    });
+    saveArr();
+
+    addNewListItem(text, dateObj);
+    clearForm();
 }
 
-function addListItem(text) {
-    //add a new <li> element
-    //add the text to that <li>
-
-    //create <span> 
-    //apply "del" class attribute
-    //apply "removeTarget()" call onclick event
-    //add innerHTML - an X emoji
-
-    //add <li> to list (where id=list)
-    let listItem = document.createElement("li");
-
-    let pText = document.createElement("p");
-    pText.innerHTML = text;
-    listItem.appendChild(pText);
+function addNewListItem(text, date) {
+    let listItem = document.createElement("div");
+    listItem.setAttribute("class", "list-item col-md-4 col-sm-12");
+    listItem.style["background-color"] = `rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}, .1)`;
 
     let spanItem = document.createElement("span");
     spanItem.setAttribute("class", "delete-button");
     spanItem.setAttribute("onclick", "removeTarget()");
-    spanItem.innerHTML = `&#215;`;
+    spanItem.innerHTML = `<i class="far fa-trash-alt"></i>`;
     listItem.appendChild(spanItem);
+
+    let txtWrapper = document.createElement("div");
+    txtWrapper.setAttribute("class", "text-wrapper");
+    let pText = document.createElement("p");
+    pText.innerHTML = text;
+    txtWrapper.appendChild(pText);
+    listItem.appendChild(txtWrapper);
+
+    let tsDiv = document.createElement("div");
+    tsDiv.setAttribute("class", "time-stamp");
+
+    let dateSpan = document.createElement("span");
+    dateSpan.innerHTML = date.date; //`${date.date.getDate()}/${(date.date.getMonth() + 1)}/${date.date.getFullYear()}`;
+    let timeSpan = document.createElement("span");
+    timeSpan.innerHTML = date.time; //`${date.time.getHours()}:${date.time.getMinutes()}`;
+
+    tsDiv.appendChild(dateSpan);
+    tsDiv.appendChild(timeSpan);
+    listItem.appendChild(tsDiv);
 
     document.getElementById("list").appendChild(listItem);
 }
 
 function removeTarget() {
-    //get the parent of the target (X button)
-    //remove parent element
-    var item = event.target.parentNode;
+    var item = event.currentTarget.parentNode;
+    let itemIndex = Array.from(item.parentNode.children).indexOf(item);
+    notesArr.splice(itemIndex, 1);
+    saveArr();
     document.getElementById("list").removeChild(item);
 }
